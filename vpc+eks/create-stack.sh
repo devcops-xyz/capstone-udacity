653dfb4e240f8681201c55e901a25b6d51ae4ac6
khaledalhasan <khaled.alhasan4@gmail.com>
2020-06-29 15:58:16 +0200

    Change parameter type to cli input json

--- vpc+eks/create-stack.sh
+++ vpc+eks/create-stack.sh
@@ -1,9 +1,9 @@
 #Stack name is inside the JSON parameters file
 #Create the infrastructure and the eks cluster
-aws cloudformation create-stack --region=us-west-2 --template-body file://eks-capstone-cluster.yaml --parameters file://eks-capstone-parameters.json --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --region=us-west-2 --template-body file://eks-capstone-cluster.yaml --cli-input-json file://eks-capstone-parameters.json --capabilities CAPABILITY_IAM
 
 #Update stack if needed
-#aws cloudformation update-stack --region=us-west-2 file://eks-capstone-cluster.yaml --parameters file://eks-capstone-parameters.json --capabilities CAPABILITY_IAM
+#aws cloudformation update-stack --region=us-west-2 file://eks-capstone-cluster.yaml --cli-input-json file://eks-capstone-parameters.json --capabilities CAPABILITY_IAM
 
 #Delete stack if needed
-#aws cloudformation delete-stack file://eks-capstone-parameters.json
+#aws cloudformation delete-stack --cli-input-json file://eks-capstone-parameters.json
